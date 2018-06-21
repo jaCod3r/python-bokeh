1e15f8db01b0e3827000b1060e006cd83147af48
jaCod3r <a.juber@hotmail.com>
2018-06-21 16:52:47 -0400

    Updated draw.py and added graph.py

--- src/draw.py
+++ src/draw.py
@@ -1,36 +1,43 @@
 import math
+import graph
 
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
+from bokeh.plotting import figure
+from bokeh.io import show, output_file
 
-N = 6
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
+
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1, 1.1), y_range=(-1.1, 1.1),
-              tools='', toolbar_location=None)
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
+
+
+plot = figure(title="Graph Layout Demonstration", x_range=(0, 500), y_range=(0, 500),
+              tools="", toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(
-    height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color="color")
 
 graph.edge_renderer.data_source.data = dict(
-    start=node_indices,
-    end=[6, 4, 5, 1, 3])
+    start=[0]*N,
+    end=node_indices)
 
-# start of layout code
-circ = [i*2*math.pi/6 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
-
-graph_layout = dict(zip(node_indices, zip(x, y)))
-graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+### start of layout code
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
 
 plot.renderers.append(graph)
 
-output_file('graph.html')
+output_file("graph.html")
 show(plot)
---
+++ src/graph.py
@@ -0,0 +1,49 @@
+import random
+
+class Edge:
+    def __init__(self, destination):
+        self.destination = destination
+
+class Vertex:
+    def __init__(self, value, **pos):
+        self.value = value
+        self.color = 'white'
+        self.pos = pos
+        self.edges = []
+    
+class Graph:
+    def __init__(self):
+        self.vertexes = []
+    def debug_create_test_data(self):
+        debug_vertex_1 = Vertex('t1', x = 40, y = 40)
+        debug_vertex_2 = Vertex('t2', x = 140, y = 140)
+        debug_vertex_3 = Vertex('t3', x = 300, y = 400)
+        debug_vertex_4 = Vertex('t4', x = 400, y = 150)
+        
+        debug_edge_1 = Edge(debug_vertex_2)
+        debug_vertex_1.edges.append(debug_edge_1)
+
+        debug_edge_2 = Edge(debug_vertex_2)
+        debug_vertex_3.edges.append(debug_edge_2)
+
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4])
+    
+    def bfs(self,start):
+        random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+        queue = []
+        found = []
+
+        start.color = random_color
+
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    found.append(edge.destination)
+                    edge.destination = random_color
+            queue.pop(0)
+
+        
+        return found
